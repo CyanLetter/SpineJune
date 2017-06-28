d602458030fe1f5d12b618d03817fd9d09582062
Dakota Ling <ourthinking@gmail.com>
2017-06-28 01:12:49 -0400

    Built switches and gates
    Revised physics blocking, added all switches and gates to scene

---
+++ Assets/Scripts/JumpSwitch.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpSwitch : MonoBehaviour {
+
+	public SwitchGate gate;
+	Animator anim;
+	bool triggered = false;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+	}
+	
+	void OnTriggerEnter2D(Collider2D other) {
+		if (!triggered) {
+			triggered = true;
+			gate.Unlock();
+			anim.SetTrigger("activate");
+		}
+	}
+}
---
+++ Assets/Scripts/SwitchGate.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchGate : MonoBehaviour {
+
+	public SpriteRenderer[] locks;
+	public Color unlockColor;
+	Animator anim;
+	int locksTriggered = 0;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+	}
+
+	public void Unlock() {
+		locks[locksTriggered].color = unlockColor;
+		locksTriggered += 1;
+		if (locksTriggered >= locks.Length) {
+			anim.SetTrigger("activate");
+		}
+	}
+}
